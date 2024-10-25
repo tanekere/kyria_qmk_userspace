c6166153b5d610d3d89758111ea021424355b097
Tejas Shastry <48762726+tanekere@users.noreply.github.com>
2024-10-25 12:51:40 +0000

    replace old keyboard code with vial code

--- keyboards/splitkb/kyria/rev3/keymaps/vial/config.h
+++ keyboards/splitkb/kyria/rev3/keymaps/vial/config.h
@@ -14,24 +14,32 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+// #pragma once
+
+// #ifdef RGBLIGHT_ENABLE
+// #    define RGBLIGHT_EFFECT_BREATHING
+// #    define RGBLIGHT_EFFECT_RAINBOW_MOOD
+// #    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+// #    define RGBLIGHT_EFFECT_SNAKE
+// #    define RGBLIGHT_EFFECT_KNIGHT
+// #    define RGBLIGHT_EFFECT_CHRISTMAS
+// #    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+// #    define RGBLIGHT_EFFECT_RGB_TEST
+// #    define RGBLIGHT_EFFECT_ALTERNATING
+// #    define RGBLIGHT_EFFECT_TWINKLE
+// #    define RGBLIGHT_HUE_STEP 8
+// #    define RGBLIGHT_SAT_STEP 8
+// #    define RGBLIGHT_VAL_STEP 8
+// #    ifndef RGBLIGHT_LIMIT_VAL
+// #    	define RGBLIGHT_LIMIT_VAL 150
+// #    endif
+// #endif
+
 #pragma once
 
-#ifdef RGBLIGHT_ENABLE
-#    define RGBLIGHT_EFFECT_BREATHING
-#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#    define RGBLIGHT_EFFECT_SNAKE
-#    define RGBLIGHT_EFFECT_KNIGHT
-#    define RGBLIGHT_EFFECT_CHRISTMAS
-#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
-#    define RGBLIGHT_EFFECT_RGB_TEST
-#    define RGBLIGHT_EFFECT_ALTERNATING
-#    define RGBLIGHT_EFFECT_TWINKLE
-#    define RGBLIGHT_HUE_STEP 8
-#    define RGBLIGHT_SAT_STEP 8
-#    define RGBLIGHT_VAL_STEP 8
-#    ifndef RGBLIGHT_LIMIT_VAL
-#    	define RGBLIGHT_LIMIT_VAL 150
-#    endif
-#endif
+#define VIAL_KEYBOARD_UID {0x04, 0x7F, 0x41, 0xF3, 0x39, 0xD8, 0x1F, 0xEA}
 
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 6, 6 }
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+#define VIAL_TAP_DANCE_ENTRIES 20
--- keyboards/splitkb/kyria/rev3/keymaps/vial/rules.mk
+++ keyboards/splitkb/kyria/rev3/keymaps/vial/rules.mk
@@ -1,6 +1,21 @@
-OLED_ENABLE = yes
-ENCODER_ENABLE = yes       # Enables the use of one or more encoders
-RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
-RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
-CONVERT_TO = something_invalid
+# OLED_ENABLE = yes
+# ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+# RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
+# RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+# CONVERT_TO = something_invalid
 
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+
+OLED_ENABLE = yes
+RGB_MATRIX_ENABLE = no
+
+ENCODER_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+
+MOUSEKEY_ENABLE		= no
+QMK_SETTINGS		= no
+COMBO_ENABLE		= no
+KEY_OVERRIDE_ENABLE	= no
+
+CONVERT_TO = kb2040
